2121e2fd767659c60d811b9f5d758e363b9e3c56
Penghui Liao <liaoishere@gmail.com>
2020-03-11 15:31:00 +0800

    Add caddy service
    Signed-off-by: Penghui Liao <liaoishere@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,18 @@ BASEPATH="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 source ${BASEPATH}/config.ini
 
 if [[ -z ${DOMAIN_NAME} || -z ${PASSWORD} || -z ${CF_Token} || -z ${CF_Account_ID} ]]; then
-    echo "Please fillin all items in config.ini" &>2
+    echo "Please fillin all items in config.ini" >&2
     exit 1
 fi
 
 V2RAY_CONFIG=${BASEPATH}/v2ray-config.json
+CADDY_FILE=${BASEPATH}/Caddyfile
 COMPOSE_FILE=${BASEPATH}/docker-compose.yml
 
-sed -ri "s@\\$\{DOMAIN_NAME}@${DOMAIN_NAME}@g" $V2RAY_CONFIG
+sed -ri "s@\\$\{DOMAIN_NAME}@${DOMAIN_NAME}@g" $CADDY_FILE
 
 sed -ri "s@\\$\{V2RAY_CONFIG}@${V2RAY_CONFIG}@g" $COMPOSE_FILE
+sed -ri "s@\\$\{CADDY_FILE}@${CADDY_FILE}@g" $COMPOSE_FILE
 sed -ri "s@\\$\{PASSWORD}@${PASSWORD}@g" $COMPOSE_FILE
 sed -ri "s@\\$\{DOMAIN_NAME}@${DOMAIN_NAME}@g" $COMPOSE_FILE
 sed -ri "s@\\$\{CF_Token}@${CF_Token}@g" $COMPOSE_FILE
